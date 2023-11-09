1ed6281a1b0d0a13c791eada243b17f12de6bd8b
Kirra-acc <143896308+Kirra-acc@users.noreply.github.com>
2023-11-09 16:07:19 +0000

    Deploying to gh-pages from @ Kirra-acc/goit-js-hw-09@1ce45bbaa0018d149abe91e1dad7c2a250df978d 🚀

---
+++ 01-color-switcher.4d99f5a6.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.querySelector("body"),o=null;t.addEventListener("click",(function(){o=setInterval((function(){n.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3),t.setAttribute("disabled",!0),e.removeAttribute("disabled")})),e.addEventListener("click",(function(){clearInterval(o),t.removeAttribute("disabled"),e.setAttribute("disabled",!0)}))}();
+//# sourceMappingURL=01-color-switcher.4d99f5a6.js.map
